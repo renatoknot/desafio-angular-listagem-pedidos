67f432d223f2e7dcd8be89e933a6ef69d26c68f4
Renato Gonçalves dos Santos <renato.santos@s2grupo.com.br>
2025-09-18 18:15:15 -0300

    Finalizado modal detalhe de pedido

--- src/app/components/pedido-detalhe/pedido-detalhe.ts
+++ src/app/components/pedido-detalhe/pedido-detalhe.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { Pedido } from '../../models/pedido.model';
 
 @Component({
   selector: 'app-pedido-detalhe',
-  imports: [],
+  imports: [CommonModule, MatDialogModule, MatButtonModule, MatListModule],
   templateUrl: './pedido-detalhe.html',
   styleUrl: './pedido-detalhe.scss',
 })
-export class PedidoDetalheComponent {}
+export class PedidoDetalheComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: Pedido) {}
+}
